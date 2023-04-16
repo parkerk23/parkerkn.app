956d3b92ce0ea48e1bca64c2d9e48f80db00f5de
parkerk23 <93094062+parkerk23@users.noreply.github.com>
2023-04-16 17:03:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const path = require('path');
+const app = express();
 
 // Set up static files
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Set up a route for the home page
 app.get('/', (req, res) => {
-  res.render('index');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Start the server
