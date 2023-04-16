ab96a9e2808e1ac537ca71bc1a1e4cbef785ab5d
parkerk23 <93094062+parkerk23@users.noreply.github.com>
2023-04-16 17:27:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(80, () => {
+  console.log('Server started on port 80');
 });
